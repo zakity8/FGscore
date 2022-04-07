2b480b5d78777936d6548c0b92104b918899a087
zakity8 <95072014+zakity8@users.noreply.github.com>
2022-04-07 18:58:09 +0900

    Add files via upload

--- js/display.js
+++ js/display.js
@@ -1,118 +1,93 @@
+let pTables;
+let tTables;
 let tt1;
 let tt3;
-
 let pt1;
 let pt3;
 
 let select;
-let pNameName;
+
+let form1;
+let form2;
+
+let player;
 let tName;
-let name0;
-let name1;
-let name2;
-let name3;
-let name4;
-let namet;
-let pTables;
-let tTables;
+let member1;
+let member2;
+let member3;
+let member4;
+
 window.addEventListener("load", function() {
     console.log('ページの読み込みが完了');
+
+    pTables = document.getElementById('form1Tables');
+    tTables = document.getElementById('form2Tables');
+
     tt1 = document.getElementById('tTable1');
     tt3 = document.getElementById('tTable3');
     pt1 = document.getElementById('pTable1');
     pt3 = document.getElementById('pTable3');
+
     select = document.getElementById('select');
-    pName = document.getElementById('personalName');
-    tName = document.getElementById('teamName');
-    name0 = document.getElementById('name0')
-    name1 = document.getElementById('name1')
-    name2 = document.getElementById('name2')
-    name3 = document.getElementById('name3')
-    name4 = document.getElementById('name4')
-    namet = document.getElementById('namet')
-    pTables = document.getElementById('personalTables');
-    tTables = document.getElementById('teamTables');
+
+    form1 = document.getElementById('form1');
+    form2 = document.getElementById('form2');
+
+    player = document.getElementById('player')
+    tName = document.getElementById('teamName')
+    member1 = document.getElementById('member1')
+    member2 = document.getElementById('member2')
+    member3 = document.getElementById('member3')
+    member4 = document.getElementById('member4')
+
     select.style.display = 'block'
 });
 
 function tohome() {
     location.reload();
-    /*
-    name0.value = '';
-    name1.value = '';
-    name2.value = '';
-    name3.value = '';
-    name4.value = '';
-    for (let i = 1; i < 11; i++){
-        for (let j = 1; j < 12; j++){
-            if(j<11){
-                pt1.rows[i].cells[j].style.backgroundColor = '';
-                pt1.rows[i].cells[j].style.pointerEvents = 'auto';
-            }
-            else pt1.rows[i].cells[j].firstChild.data = 0;
-        }
-    }
-    pt3.rows[1].cells[1].firstChild.data = 0;
-
-    for (let s = 1; s < 41; s++){
-        for (let t = 1; t < 12; t++){
-            if(s%4 != 1 && t < 11){
-                tt1.rows[s].cells[t].style.backgroundColor = '';
-                tt1.rows[s].cells[t].style.pointerEvents = 'auto';
-            }
-            else pt1.rows[i].cells[j].firstChild.data = 0;
-        }
-    }
-    tt3.rows[1].cells[1].firstChild.data = 0;
-
-    select.style.display = 'block'
-    pName.style.display = 'none';
-    tName.style.display = 'none';
-    pTables.style.display = 'none';
-    tTables.style.display = 'none';*/
 }
 
-function personalClick() {
+function showForm1() {
     select.style.display = 'none';
-    pName.style.display = 'block';
+    form1.style.display = 'block';
 }
-function teamClick() {
+function showForm2() {
     select.style.display = 'none';
-    tName.style.display = 'block';
+    form2.style.display = 'block';
 }
 function showPtable() {
-    if (!(name0.value)){
+    if (!(player.value)){
         alert('配信者の名前を入力してください')
     }
     else {
-        pt1.rows[0].cells[0].firstChild.data = name0.value;
-        pt3.rows[1].cells[0].firstChild.data = name0.value;
-        pName.style.display = 'none';
+        pt1.rows[0].cells[0].firstChild.data = player.value;
+        pt3.rows[1].cells[0].firstChild.data = player.value;
+        form1.style.display = 'none';
         pTables.style.display = 'block';
     }
 }
 
 function showTtable() {
-    if (!(namet.value) || !(name1.value) || !(name2.value) || !(name3.value) || !(name4.value)){
+    if (!(tName.value) || !(member1.value) || !(member2.value) || !(member3.value) || !(member4.value)){
         alert('すべて入力してください')
     }
     else {
         for(let k = 1; k < 41; k++) {
             if(k%4 == 1)
-                tt1.rows[k].cells[1].firstChild.data = name1.value;
+                tt1.rows[k].cells[1].firstChild.data = member1.value;
             else if(k%4 == 2)
-                tt1.rows[k].cells[0].firstChild.data = name2.value;
+                tt1.rows[k].cells[0].firstChild.data = member2.value;
             else if(k%4 == 3)
-                tt1.rows[k].cells[0].firstChild.data = name3.value;
+                tt1.rows[k].cells[0].firstChild.data = member3.value;
             else
-                tt1.rows[k].cells[0].firstChild.data = name4.value;
+                tt1.rows[k].cells[0].firstChild.data = member4.value;
         }
-        tt3.rows[1].cells[0].firstChild.data = name1.value;
-        tt3.rows[2].cells[0].firstChild.data = name2.value;
-        tt3.rows[3].cells[0].firstChild.data = name3.value;
-        tt3.rows[4].cells[0].firstChild.data = name4.value;
-        tt1.rows[0].cells[0].firstChild.data = namet.value;
-        tName.style.display = 'none';
+        tt3.rows[1].cells[0].firstChild.data = member1.value;
+        tt3.rows[2].cells[0].firstChild.data = member2.value;
+        tt3.rows[3].cells[0].firstChild.data = member3.value;
+        tt3.rows[4].cells[0].firstChild.data = member4.value;
+        tt1.rows[0].cells[0].firstChild.data = tName.value;
+        form2.style.display = 'none';
         tTables.style.display = 'block';
     }
 }
